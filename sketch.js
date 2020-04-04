c35b9970b4c6aa709e227baf04ea8b9af7adb53e
PrajwalBaxi <63157936+PrajwalBaxi@users.noreply.github.com>
2020-04-04 20:59:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  trex_collided = loadImage("trex_collided.png");
+  
+  groundImage = loadImage("ground2.png")
+}
+
+function setup() {
+  createCanvas(600, 200);
+  
+  trex = createSprite(50,180,20,50);
+  trex.addAnimation("running", trex_running);
+  trex.scale = 0.5;
+  
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+  ground.velocityX = -2;
+  
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+}
+
+function draw() {
+  background(220);
+  
+  if(keyDown("space")) {
+    trex.velocityY = -10;
+  }
+  
+  trex.velocityY = trex.velocityY + 0.8
+  
+  if (ground.x < 0){
+    ground.x = ground.width/2;
+  }
+  
+  trex.collide(invisibleGround);
+  drawSprites();
+}
